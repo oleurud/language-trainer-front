1a1279b7eb85a79c535d8f559498457aaa5e1186
Simon Martín <oleurud@gmail.com>
2018-11-23 22:01:56 +0100

    home logged

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,22 @@
 <template>
-    <div class="home">
+    <Courses v-if="user"></Courses>
+    <div class="home" v-else>
         <h1>Welcome</h1>
     </div>
 </template>
 
 <script>
+import Courses from '@/components/pages/courses/Courses'
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    computed: {
+        user() {
+            return this.$store.state.user
+        }
+    },
+    components: {
+        Courses
+    }
 }
 </script>
